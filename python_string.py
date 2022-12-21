63b6f9ce2bdc5466e5a81dbd6a30f84123d77b70
Khemnath <khemnath.devops@outlook.com>
2022-12-21 15:08:18 +0800

    Sample python program with string usage

---
+++ python_string.py
@@ -0,0 +1,14 @@
+# we can create a string by surrounding some text with quotation marks:
+# We can use either single quotes (string1) or double quotes (string2) to create a string as long as you use the same type at the beginning
+# and end of the string.
+
+# Assigning a string to a variable
+string1 = 'This is string'
+string2 = "3600"
+
+# Concatenating strings: You can concatenate (combine) strings using the + operator:
+
+greeting = "Hi, "
+name = "Trinay"
+full_message = greeting + name
+print(full_message) 
