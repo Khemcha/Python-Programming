c9249b3dc6f5efafb8658e715d5685af5f906464
Khemnath <khemnath.devops@outlook.com>
2022-12-21 11:03:21 +0800

    Sample python with variable usage

---
+++ python_variable.py
@@ -0,0 +1,5 @@
+# variables are names that can be assigned a value and then used to refer to that value throughout your code.
+# Values are assigned to variable names using a special symbol called the assignment operator (=) . The = operator takes the value to the
+# right of the operator and assigns it to the name on the left.
+name="Trinay Chauhan"
+print(name)
